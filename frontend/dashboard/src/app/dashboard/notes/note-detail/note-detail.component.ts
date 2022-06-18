ccd52e06752d5775050a390c57215b519c3e2d88
shanart <shanin.x@gmail.com>
2022-06-19 01:29:06 +0300

    CU-32b7qh7[complete] remove all old

--- frontend/dashboard/src/app/dashboard/notes/note-detail/note-detail.component.ts
+++ frontend/dashboard/src/app/dashboard/notes/note-detail/note-detail.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-note-detail',
-  templateUrl: './note-detail.component.html',
-  styleUrls: ['./note-detail.component.scss']
+  templateUrl: './note-detail.component.html'
 })
 export class NoteDetailComponent implements OnInit {
 
