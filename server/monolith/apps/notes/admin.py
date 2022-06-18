f257dcab042c755f0a0f58a7b91911f6789ac4dd
shanart <shanin.x@gmail.com>
2022-06-18 22:11:24 +0300

    content seeder ( without tags, categories and entity meta )

--- server/monolith/apps/notes/admin.py
+++ server/monolith/apps/notes/admin.py
@@ -4,8 +4,9 @@ from apps.notes.models import Note
 
 class NoteAdmin(admin.ModelAdmin):
     search_fields = ("title", )
-    list_display = ("title", "created_at", )
+    list_display = ("title", "owner", "created_at", )
     readonly_fields = ("created_at", "updated_at", )
+    list_filter = ("owner", "category", )
 
 
 admin.site.register(Note, NoteAdmin)
